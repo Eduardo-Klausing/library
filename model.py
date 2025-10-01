12495b7e657d57d7677ffa5264e770b1df106480
Eduardo Klausing <eduardokgmuniz@gmail.com>
2025-09-30 23:01:29 -0300

    Commit 3: Extracting method get_charge from Client.statement.

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-# model.py
-
 class Book:
     REGULAR: int = 0
     NEW_RELEASE: int = 1
@@ -21,49 +19,54 @@ class Rental:
 
     # MÉTODOS @property REMOVIDOS
 
-
 class Client:
     def __init__(self, name: str):
-        # ATRIBUTOS RENOMEADOS (sem _)
         self.name = name
         self.rentals = []
 
     def add_rental(self, rental: Rental):
-        # ATRIBUTO RENOMEADO AQUI TAMBÉM
         self.rentals.append(rental)
 
-    # MÉTODO @property REMOVIDO
+    def get_charge(self, rental: Rental) -> float:
+        """
+        Calcula o valor de um aluguel específico.
+        Este é o método extraído.
+        """
+        amount = 0
+        
+        # Lógica de cálculo do valor movida para cá
+        if rental.book.price_code == Book.REGULAR:
+            amount += 2
+            if rental.days_rented > 2:
+                amount += (rental.days_rented - 2) * 1.5
+        elif rental.book.price_code == Book.NEW_RELEASE:
+            amount += rental.days_rented * 3
+        elif rental.book.price_code == Book.CHILDREN:
+            amount += 1.5
+            if rental.days_rented > 3:
+                amount += (rental.days_rented - 3) * 1.5
+        
+        return amount
 
     def statement(self) -> str:
         total_amount = 0
         frequent_renter_points = 0
-        # O ACESSO a self.name já era assim, então não muda.
         result = f"Rental summary for {self.name}\n"
         
-        # ATRIBUTO RENOMEADO AQUI (de _rentals para rentals)
         for rental in self.rentals:
-            amount = 0
-            
-            # O acesso a rental.book.price_code não muda, pois
-            # do ponto de vista do código "cliente", a sintaxe é a mesma.
-            if rental.book.price_code == Book.REGULAR:
-                amount += 2
-                if rental.days_rented > 2:
-                    amount += (rental.days_rented - 2) * 1.5
-            elif rental.book.price_code == Book.NEW_RELEASE:
-                amount += rental.days_rented * 3
-            elif rental.book.price_code == Book.CHILDREN:
-                amount += 1.5
-                if rental.days_rented > 3:
-                    amount += (rental.days_rented - 3) * 1.5
+            # O bloco 'if/elif/else' foi substituído por esta chamada de método
+            amount = self.get_charge(rental)
 
+            # add frequent renter points
             frequent_renter_points += 1
             if rental.book.price_code == Book.NEW_RELEASE and rental.days_rented > 1:
                 frequent_renter_points += 1
 
+            # show each rental result
             result += f"- {rental.book.title}: {amount}\n"
             total_amount += amount
         
+        # show total result
         result += f"Total: {total_amount}\n"
         result += f"Points: {frequent_renter_points}"
         return result
